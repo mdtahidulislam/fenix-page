3d550abb76445c9c2cde69743d574285bdabae4d
mdtahidulislam <tahidul.developer@gmail.com>
2022-09-14 02:47:41 +0600

    first

---
+++ dist/assets/js/main.js
@@ -0,0 +1,78 @@
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+      
+
+      // home slider 
+
+      const swiper = new Swiper(".home-slider", {
+        // effect: 'fade',
+        // fadeEffect: {
+        //   crossFade: true
+        // },
+        loop: true,
+        speed: 2500,
+        // autoplay: {
+        //   delay: 2500,
+        //   disableOnInteraction: true,
+        // },
+        pagination: {
+          el: ".swiper-pagination",
+          type: "bullets",
+          clickable: true,
+        },
+      });
+      // $(".home-slider").mouseenter(function () {
+      //   swiper.autoplay.stop();
+      // });
+      // $(".home-slider").mouseleave(function () {
+      //   swiper.autoplay.start();
+      // });
+
+      // menu 
+      $('.js-menu-bar').on('click', function() {
+        $('.js-header-left').toggle();
+        $('.js-header-right').toggle();
+      });
+
+      
+      $('.js-mobile-menu-items li').each(function(idx, elm ) {
+        const delay = idx * 0.05;
+        $(this).css({'transitionDelay': `${delay}s`});
+      });
+
+      $('.js-menu-bar').on('click', function(){
+        $('.js-mobile-menu').addClass('open');
+      });
+      $('.js-mobile-menu-close').on('click', function(){
+        $('.js-mobile-menu').removeClass('open');
+      });
+      
+      
+      // const tabelements = [
+      //   {
+      //     id: 'monthTab',
+      //     triggerEl: $('#month-tab'),
+      //     targetEl: $('#month')
+      //   },
+      //   {
+      //     id: 'yearTab',
+      //     triggerEl: $('#year-tab'),
+      //     targetEl: $('#year')
+      //   }
+      // ]
+      // const options = {
+      //   defaultTabId: 'yearTab',
+      //   activeClasses: 'text-red-600',
+      //   inactiveClasses: 'text-gray-500 ',
+      //   onShow: () => {
+      //     console.log('tab is shown');
+      //   }
+      // }
+      // const tabs = new Tabs(tabelements, options);
+      // tabs.show('yearTab');
+
+    });
+
+}(jQuery));
---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+    content: ['./dist/**/*.{html,js}'],
+    theme: {
+      extend: {
+        fontFamily: {
+          gothamlight: 'gothamlight, sans-serif',
+          gothammedium: 'gothammedium, sans-serif',
+          gothambold: 'gothambold, sans-serif',
+          luxiadisplay: 'luxiadisplay, sans-serif',
+          luxiaregular: 'luxiaregular, sans-serif',
+        },
+        colors: {
+          gold1: '#a78960',
+          gold2: '#E7D6B7',
+          gold3: '#F9F3E8',
+          gold4: '#E5C7E2',
+          fuchsia1: '#350937',
+          fuchsia2: '#764E71',
+          slate1: '#201e3b',
+          slate2: '#645D74',
+          cyan1: '#0f2e3d',
+          cyan2: '#516C76',
+          white1: '#f9f3e8',
+        },
+        backgroundImage: {
+          'hero-banner': "url('../images/home/banner.jpg')",
+          'slider-bg-1': "url('../images/slider/slider-1.jpeg')",
+          'slider-bg-2': "url('../images/slider/slider-2.jpeg')",
+          'slider-bg-3': "url('../images/slider/slider-3.jpeg')",
+          'slider-bg-4': "url('../images/slider/slider-4.jpeg')",
+        },
+        screens: {
+          fourkxl: {'min': '2500px'},
+          fourk: {'min': '2100px'},
+          fourklg: {'min': '1800px'},
+          fourkm: {'min': '1600px'},
+          fourksm: {'min': '1281px'},
+          maxxl: {'max': '1280px'},
+          maxlg: {'max': '1024px'},
+          maxdp: {'max': '992px'},
+          maxmd: {'max': '768px'},
+          maxsm: {'max': '640px'},
+          maxxsmm: {'max': '576px'},
+          maxxsml: {'max': '450px'},
+          maxxsms: {'max': '375px'},
+        }
+      },
+    },
+    plugins: [],
+  }
+  
